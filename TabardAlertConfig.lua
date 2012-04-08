c5f6e4c5c0fad43b1c38dc63d4c73788dffa7a8d
Jack Scholting <jas2m8@mst.edu>
2012-04-08 17:50:17 -0500

    the tabard to faction table in now finished

--- TabardAlertConfig.lua
+++ TabardAlertConfig.lua
@@ -4,29 +4,21 @@
 --      Interface->Addons->TabardAlert.
 -------------------------------------------------------------------------------
 
+-- Create the frame that will exist in the Interface Options Panel.
 TabardAlertPanel = CreateFrame( "Frame", "TabardAlertPanel", UIParent );
+-- Give the frame a name in the listing on the left.
 TabardAlertPanel.name = "TabardAlert";
+-- Add this frame inside the Interface Options Panel.
 InterfaceOptions_AddCategory(TabardAlertPanel);
 
+-- Create a Checkbutton.
 local frame = CreateFrame("CheckButton", "UICheckButtonTemplateTest", TabardAlertPanel, "UICheckButtonTemplate")
 frame:ClearAllPoints()
 frame:SetPoint("CENTER", 0, 0)
 _G[frame:GetName() .. "Text"]:SetText("Example checkbutton")
         
-function TabardAlertConfig_OnLoad(panel)
-        -- Set the name for the Category for the Panel
-        panel.name = "TabardAlert";
-        
+-- When the player clicks okay, run this function.
+-- panel.okay = function (self) SC_ChaChingPanel_Close(); end;
 
-        -- When the player clicks okay, run this function.
-        --
-        -- panel.okay = function (self) SC_ChaChingPanel_Close(); end;
-
-        -- -- When the player clicks cancel, run this function.
-        -- --
-        -- panel.cancel = function (self)  SC_ChaChingPanel_CancelOrLoad();  end;
-
-        -- Add the panel to the Interface Options
-        --
-        InterfaceOptions_AddCategory(panel);
-end
+-- -- When the player clicks cancel, run this function.
+-- panel.cancel = function (self)  SC_ChaChingPanel_CancelOrLoad();  end;
