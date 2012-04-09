02c4cd2d8e688a27b97d4979bf65d05c5334d17b
Jack Scholting <jas2m8@mst.edu>
2012-04-08 21:06:40 -0500

    Added gui material to interace config, working first checkbutton

--- TabardAlertConfig.lua
+++ TabardAlertConfig.lua
@@ -26,13 +26,37 @@ InterfaceOptions_AddCategory(TabardAlertPanel);
 -- panel.cancel = function (self)  SC_ChaChingPanel_CancelOrLoad();  end;
 -------------------------------------------------------------------------------
 
----------------------------
--- Create the checkboxes --
+-----------
+-- Title --
 -------------------------------------------------------------------------------
--- Create a Checkbutton.
-local frame = CreateFrame("CheckButton", "UICheckButtonTemplateTest", TabardAlertPanel, "UICheckButtonTemplate")
-frame:ClearAllPoints()
-frame:SetPoint("CENTER", 0, 0)
-_G[frame:GetName() .. "Text"]:SetText("Example checkbutton")
+local title = TabardAlertPanel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+title:SetPoint("TOPLEFT", 16, -16)
+title:SetText("TabardAlert Configuration")
+-------------------------------------------------------------------------------
+
+----------------
+-- Checkboxes --
+-------------------------------------------------------------------------------
+-- Create the Checkbutton.
+local frame1 = CreateFrame("CheckButton", "EnabledConfigCheckbutton", TabardAlertPanel, "UICheckButtonTemplate")
+frame1:ClearAllPoints()
+-- Set the position to be at the top left. 5 pixels to the right, 60 pixels down from the parent.
+frame1:SetPoint("TOPLEFT", 5, -60)
+-- Set the text for the checkbutton.
+_G[frame1:GetName() .. "Text"]:SetText("Enable Addon")
+-- Create an event handler to handle when the checkbutton is actually checked.
+frame1:SetScript("PostClick", function(self, button, down)
+    print("it worked");
+end)
+
+local frame2 = CreateFrame("CheckButton", "EnableSoundConfigCheckbutton", TabardAlertPanel, "UICheckButtonTemplate")
+frame2:ClearAllPoints()
+frame2:SetPoint("TOPLEFT", 5, -140)
+_G[frame2:GetName() .. "Text"]:SetText("Enable Sound - Play an audible sound when reputation is wasted.")
+
+local frame3 = CreateFrame("CheckButton", "MaxExaltedConfigCheckbutton", TabardAlertPanel, "UICheckButtonTemplate")
+frame3:ClearAllPoints()
+frame3:SetPoint("TOPLEFT", 5, -100)
+_G[frame3:GetName() .. "Text"]:SetText("Max Exalted - Do not give alerts until the exalted bar is maxed at 999/1000.")
 -------------------------------------------------------------------------------
         
